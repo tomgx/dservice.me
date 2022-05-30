8ffcc206fed669c17d99f7eafbbf78a69b5d6b21
Thomas <t.green01q@gmail.com>
2022-05-30 14:20:39 +0100

    Homepage text color change minor change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,16 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#EEEEEE]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <h2 className='cursor-default text-center text-4xl sm:text-7xl font-bold text-[#646464] hover:text-[#54aa97]'>
-          dservice
+        <h2 className='text-center text-4xl sm:text-7xl font-bold'>
+         <a className='cursor-default text-[#646464] hover:text-[#54aa97]'>
+           dservice
+           </a> 
         </h2>
-        <p className='cursor-default text-center font-bold text-[#acacac] hover:text-[#646464] pl-56'>tgreen</p>
+        <p className='text-center font-bold pl-56'>
+         <a className='cursor-default text-[#acacac] hover:text-[#646464]'>
+           tgreen
+           </a> 
+          </p>
         <div className='self-center'>
         </div>
         
